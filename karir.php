2f6933e443eed9ea0c49c0fafb2aebfbe24aaab8
Buhairi <79437567+buhairi-dev@users.noreply.github.com>
2023-11-25 12:05:37 +0700

    Add files via upload

---
+++ karir.php
@@ -0,0 +1,179 @@
+<?php include ('header.php')?>
+
+<div style="background-image:url('assets/img/karir.jpg');background-repeat: repeat-space; padding-top:260px; ">
+<div class="container" style="top:-140px; position:relative;">
+<h3>Join With Us.</h3>
+<p>Mari berkarir bersama kami.</p>
+</div>
+</div>
+
+<section id="karir" class="contact">
+<div class="container" style="padding-left:10%;   ">
+<form class="row g-4">
+
+<div class="col-lg-2">
+<div class="form-group">
+<div class="position-relative">
+<label class="form-label"><strong><i class="bi bi-caret-down"></i>  Lowongan Kerja</strong></label>
+<input type="text" placeholder="Marketing"  class="form-control">
+</div>
+</div>
+</div>
+
+
+<div class="col-lg-3">
+<div class="form-group">
+<div class="position-relative">
+<label class="form-label"><strong><i class="bi bi-caret-down"></i> Kategori</strong></label>
+<select name="asal" id="inputAsal" form="inputAsal" class="form-control">
+  <option value="asal">- Kategori Pekerjaan -</option>
+  <option value="jakarta">Staf Marketing</option>
+  <option value="jakarta">Staf Administrasi</option>
+  <option value="jakarta">IT Operation</option>
+  <option value="jakarta">HRD Personalia</option>
+  <option value="jakarta">Kurir</option>
+</select>
+</div>
+</div>
+</div>
+
+<div class="col-lg-3">
+<div class="form-group">
+<div class="position-relative">
+<label class="form-label"><strong><i class="bi bi-caret-down"></i> Lokasi</strong></label>
+<select name="asal" id="inputAsal" form="inputAsal" class="form-control">
+  <option value="asal">- Kantor Cabang -</option>
+  <option value="jakarta">Jakarta</option>
+  <option value="jakarta">Manado</option>
+  <option value="jakarta">Batam</option>
+  <option value="jakarta">Makasar</option>
+  <option value="jakarta">Denpasar</option>
+</select>
+</div>
+</div>
+</div>
+
+<div class="col-lg-3">
+<div class="form-group" style="margin-top:-15px;">
+<button class="btn btn-primary" submit="reset">Cari Lowongan</button>
+</div>
+</div>
+
+</form>
+</div>
+
+
+
+</section>
+
+
+<div class="container"><hr/>
+<div class="table-responsive">
+  <table class="table table-striped">
+    <thead align="center">
+      <th style="width:50%">Jabatan</th>
+      <th>Kategori</th>
+      <th>Lokasi</th>
+      <th>More</th>
+    </thead>
+    <tbody>
+      <tr>
+        <td ><p><strong>Marketing Profresional</strong></p>
+        <p>Kualifikasi : Pria, Usia Maks. 27 Tahun, Pendidikan Min. S1 Ekonomi
+        (Diutamakan Akuntansi) Dengan Ipk Min. 2,75,
+        Memiliki Kemampuan Komunikasi Dan Analisa Yang Baik,
+        Memiliki Pengalaman Kerja Min. 2 Tahun Sebagai Auditor Kap Maupun Perusahaan</p>
+        </td>
+        <td>Staf Marketing</td>
+        <td class="align-top">Jakarta</td>
+        <td><a href="#" data-bs-toggle="modal" data-bs-target="#myModal">Detail ...</a></td>
+      </tr>
+      <tr>
+        <td><strong>Staff Administrasi / Kasir</strong>
+        <br/>
+       Wanita, Usia Maks. 27 Tahun, Pendidikan Min. S1 Ekonomi
+        (Diutamakan Akuntansi) Dengan Ipk Min. 2,75,
+        Memiliki Kemampuan Komunikasi Dan Analisa Yang Baik,
+        Memiliki Pengalaman Kerja Min. 2 Tahun Sebagai Auditor Kap Maupun Perusahaan 
+        </td>
+        <td>Staf Marketing</td>
+        <td class="align-top">Jakarta</td>
+        <td><a href="#" data-bs-toggle="modal" data-bs-target="#myModal">Detail ...</a></td>
+      </tr>
+      <tr>
+      <td><strong>Staff Administrasi / Kasir</strong>
+        <br/>
+        Wanita, Usia Maks. 27 Tahun, Pendidikan Min. S1 Ekonomi
+        (Diutamakan Akuntansi) Dengan Ipk Min. 2,75,
+        Memiliki Kemampuan Komunikasi Dan Analisa Yang Baik,
+        Memiliki Pengalaman Kerja Min. 2 Tahun Sebagai Auditor Kap Maupun Perusahaan 
+        </td>
+        <td>Staf Marketing</td>
+        <td class="align-top">Jakarta</td>
+        <td><a href="#" data-bs-toggle="modal" data-bs-target="#myModal">Detail ...</a></td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+<br/>
+ 
+ 
+<p><i style="font-size:12px;">"Waspada terhardap penipuan lowongan kerja yang mengatas namakan PT. Agung Wicaksana Utama Sakti (Agung Cargo).</i></p>
+</div>
+
+
+
+<!-- The Modal -->
+<div class="modal" id="myModal">
+  <div class="modal-dialog modal-dialog-centered modal-lg">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Detail Lowongan Kerja</h4>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+      </div>
+
+      <!-- Modal body -->
+      <div class="modal-body">
+      <h4 class="text-primary">Marketing Profesional</h4>
+
+      <p><i class="bi bi-building"></i> Lokasi : Jakarta</p>
+  
+      <p><i class="bi bi-check2-square"></i> Kualifikasi :</p>
+      <p>Wanita, Usia Maks. 27 Tahun, Pendidikan Min. S1 Ekonomi
+        (Diutamakan Akuntansi) Dengan Ipk Min. 2,75,
+        Memiliki Kemampuan Komunikasi Dan Analisa Yang Baik,
+        Memiliki Pengalaman Kerja Min. 2 Tahun Sebagai Auditor Kap Maupun Perusahaan.</p>
+
+      <p><i class="bi bi-bookmark-plus"></i> Benefit :</p>
+      <ul>
+      <li> Gaji Pokok</li>
+      <li> Insentif</li>
+      <li> Asuransi BPJS</li>
+      <li> Jenjang Karir</li>
+      </ul>
+
+      <p>Lamaran dapat ditujukan ke alamat email : <a href="mailto: hairing@agungcargo.com">hairing@agungcargo.com</a></p>
+     
+      </div>
+
+      <!-- Modal footer -->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-bs-dismiss="modal"><i class="bi bi-arrow-counterclockwise"></i> Kembali</button>
+      </div>
+
+    </div>
+  </div>
+  
+</div>
+
+
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+ 
+<?php include ('footer.php')?>
